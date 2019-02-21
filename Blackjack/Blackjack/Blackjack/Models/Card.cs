ef5ae78414e9e1d0f489b90db83927aaefee6cac
wolfordr <rwolfordc@gmail.com>
2019-02-21 10:00:49 -0500

    ViewModel and Model initial
    -Started adding prototype code into ViewModel
    -Created card Model that can store Name, value, and suit
    -Ironed out initial errors

---
+++ Blackjack/Blackjack/Blackjack/Models/Card.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Blackjack.Models
+{
+    public class Card : INotifyPropertyChanged
+    {
+        private string cardName;
+        private int cardValue;
+        private string suit;
+
+
+        public string CardName
+        {
+            get
+            {
+                return cardName;
+            }
+
+            set
+            {
+                cardName = value;
+                OnPropertyChanged("CardName");
+
+            }
+        }
+
+        public int CardValue
+        {
+            get
+            {
+                return cardValue;
+            }
+            set
+            {
+                cardValue = value;
+                OnPropertyChanged("CardValue");
+            }
+
+
+        }
+
+        public string Suit
+        {
+            get
+            {
+                return suit;
+            }
+            set
+            {
+                suit = value;
+                OnPropertyChanged("Suit");
+            }
+        }
+
+
+
+        
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+
+    }
+}
---
+++ Blackjack/Blackjack/Blackjack/ViewModels/cardsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Blackjack.Models;
+
+namespace Blackjack.ViewModels
+{
+    class CardsViewModel: INotifyPropertyChanged
+    { 
+        
+            public ObservableCollection<Card> Cards { get; set; }
+
+            public void cardsViewModel()
+            {
+                Cards = new ObservableCollection<Card>();
+
+
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public void OnPropertyChanged(string name)
+            {
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+                }
+            }
+        
+        //
+    }
+}
